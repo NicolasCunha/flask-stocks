a313810627ca7d0b1adfe97bbf1767f76b963b48
NicolasCunha <nicolascunha17@gmail.com>
2021-04-01 06:10:38 -0300

    feat: daily, weekly and monthly values

--- api/stocks.py
+++ api/stocks.py
@@ -1,16 +1,22 @@
 import requests
 import json
+import logging
+
+# The following is the URL for a symbol search (stock "code")
+SEARCH_DEFAULT_URL = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={}&apikey={}"
+DAILY_VALUE_DEFAULT_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize=compact&apikey={}"
+WEEKLY_VALUE_DEFAULT_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={}&outputsize=compact&apikey={}"
+MONTHLY_VALUE_DEFAULT_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={}&outputsize=compact&apikey={}"
 
 class Stocks:
+
     def __init__(self, api_key):
         self.api_key = api_key
 
     def get_avaiable_stocks(self, term):
-        # The following is the URL for a symbol search (stock "code")
-        url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={}&apikey={}"
         # Format the URL to contain the term to be searched, as well as the API key that was
         # defined when starting the server.
-        url = url.format(term, self.api_key)
+        url = SEARCH_DEFAULT_URL.format(term, self.api_key)
         # Execute a GET request
         result = requests.get(url)
         if(result != None):
@@ -21,14 +27,62 @@ class Stocks:
             try:
                 # Each entry in "bestMatches" is a stock
                 for match in resultDict["bestMatches"]:
-                    dictEntry = {"code": match["1. symbol"], "name" : match["2. name"], "region": match["4. region"]}
+                    dictEntry = {
+                        "code": match["1. symbol"], 
+                        "name" : match["2. name"], 
+                        "region": match["4. region"]
+                    }
                     responseDict.update({i : dictEntry})
                     i = i + 1
             except:
                 # Alpha Vantage Free API has a limit of 5 request per minute, 500 per day.
-                responseDict.update({0 : {"code": 404, "name": "API NOT AVAIABLE", "region" : "N/A"}})
+                responseDict.update({"error": True, "desc": "Api is not avaiable. Alpha Vantage Free API has a limit of 5 request per minute, 500 per day."})
             return responseDict
         # If no result was found, return a empty array
         return ['']
+
+    def copy_dict_to_self(self, tag, dict):
+        newDict = {tag : dict}
+        return newDict
+
+    def get_value_url(self, extUrl, term, tag, responseTag):
+        url = extUrl.format(term, self.api_key)
+        result = requests.get(url)
+        if (result != None):
+            resultDict = json.loads(result.text)
+            responseDict = {}        
+            i = 0
+            try:
+                # Each entry in "bestMatches" is a stock
+                for match in list(resultDict[tag].items())[:7]:
+                    values = match[1]
+                    dictEntry = {
+                        "date" : match[0],
+                        "open" : values["1. open"], 
+                        "high" : values["2. high"], 
+                        "low" : values["3. low"], 
+                        "close": values["4. close"], 
+                        "volume": values["5. volume"]
+                    }
+                    responseDict.update({i : dictEntry})
+                    i = i + 1
+                responseDict = self.copy_dict_to_self(responseTag, responseDict)
+            except Exception:
+                # Alpha Vantage Free API has a limit of 5 request per minute, 500 per day.
+                responseDict.update({"error": True, "desc": "Api is not avaiable. Alpha Vantage Free API has a limit of 5 request per minute, 500 per day."})
+                logging.exception("Exception")
+            return responseDict                
+        return ['']
+
+    def get_values(self, term):
+        result = {}
+        daily = self.get_value_url(DAILY_VALUE_DEFAULT_URL, term, "Time Series (Daily)", "daily")
+        weekly = self.get_value_url(WEEKLY_VALUE_DEFAULT_URL, term, "Weekly Time Series", "weekly")
+        monthly = self.get_value_url(MONTHLY_VALUE_DEFAULT_URL, term, "Monthly Time Series", "monthly")
+        result.update(daily)
+        result.update(weekly)
+        result.update(monthly)
+        return result
+
 
 
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from flask import Flask, Response, render_template, request, jsonify
 from flask_cors import CORS
 from api.stocks import Stocks
 
-app = Flask(__name__, template_folder = 'html/static')
+app = Flask(__name__, template_folder = 'static/html')
 
 CORS(app)
 
@@ -18,11 +18,16 @@ def search():
     stocksResult = stocks.get_avaiable_stocks(request.form.get('stock_filter'))
     return Response(json.dumps(stocksResult), mimetype='application/json')
 
+@app.route('/_get_values', methods=['POST'])
+def get_values():
+    stocksResult = stocks.get_values(request.form.get('stock_filter'))
+    return Response(json.dumps(stocksResult), mimetype='application/json')
+
 if len(sys.argv) > 1:
     apikey = sys.argv[1]
     stocks = Stocks(apikey)
     PORT = 5000
-    app.run(port = PORT)
+    app.run(port = PORT, debug=True)
 else:
     # API key can be found at https://www.alphavantage.co/support/#api-key
     print('Api key not found.')
